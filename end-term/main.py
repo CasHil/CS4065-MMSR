3f2e96371acc73b9067c5a75e8047d7e650e3270
Gustav Nobert <s185031@student.dtu.dk>
2023-06-13 19:14:01 +0200

    poc works, only returns song name atm

--- end-term/main.py
+++ end-term/main.py
@@ -5,6 +5,7 @@ import re
 import numpy as np
 import csv
 from sklearn.metrics.pairwise import cosine_distances
+from flask import Flask, request
 
 from gensim.models import Word2Vec
 import gensim.downloader as api
@@ -19,11 +20,11 @@ df.drop(['time_signature', 'duration'], axis=1)
 pd.options.display.max_columns = 10
 df.head()
 
-df1 = df.drop(['duration', 'year', 'energy', 'danceability'], axis=1)
+# df1 = df.drop(['duration', 'year', 'energy', 'danceability'], axis=1)
 
-data_final = df1.drop(['song_id', 'song_title'], axis=1)
+# data_final = df1.drop(['song_id', 'song_title'], axis=1)
 
-data_scaled = pd.DataFrame(df1, columns=['danceability', 'energy', 'loudness', 'tempo', 'time_signature', 'segment_loudness_avg',
+data_scaled = pd.DataFrame(df, columns=['danceability', 'energy', 'loudness', 'tempo', 'time_signature', 'segment_loudness_avg',
                                                 #  'chroma1', 'chroma2', 'chroma3', 'chroma4', 'chroma5', 'chroma6',
                                                 #  'chroma7', 'chroma8', 'chroma9', 'chroma10', 'chroma11', 'chroma12',
                                                 #  'MFCC1', 'MFCC2', 'MFCC3', 'MFCC4', 'MFCC5', 'MFCC6', 'MFCC7', 'MFCC8',
@@ -32,16 +33,16 @@ data_scaled = pd.DataFrame(df1, columns=['danceability', 'energy', 'loudness', '
 
 
 
-subset_columns = ['danceability', 'energy', 'loudness', 'tempo']
+subset_columns = ['loudness', 'tempo']#['danceability', 'energy', 'loudness', 'tempo']
 song_subset = data_scaled[subset_columns]
 # song_subset = (song_subset - song_subset.min()) / (song_subset.max() - song_subset.min())
-columns = ['dance', 'energy', 'loudness', 'tempo'] #'time', 'segment_loudness_avg']
-
+#columns =  #'time', 'segment_loudness_avg']
+columns = subset_columns#['dance', 'energy', 'loudness', 'tempo']
 # def normalize_song_subset(songs, columns):
 #     min_values = songs.min()
 #     max_values = songs.max()
-        
-
+normalized_songs=(song_subset-song_subset.mean())/song_subset.std()
+    
 
 def get_cosine(vec1, vec2):
     try:
@@ -64,29 +65,7 @@ def find_max_values(keywords, columns):
                     max = (distance, keyword)
             feeling_dict[column] = {"min": min[1], "max": max[1], "distance": max[0] - min[0]}
     return feeling_dict
-    
-def get_keyword_weights(keyword, distances, columns):
-    weights = {}
-    for column in columns:
-        distance = distances[column]
-        distance_to_min = abs(get_cosine(distance["min"], keyword))
-        # if distance["distance"] == 0.0:
-            # normalized_value = 1.0
-        # else:
-            # normalized_value = (distance["distance"] - distance_to_min) / distance["distance"]
-        weights[column] = distance
-    return weights
 
-## recommends a song based a vector, and the distance to a songs vector
-def recommend_song(song_vector):
-    song_vector = np.array(list(song_vector.values()))
-    distances = cosine_distances(song_subset, [song_vector])
-    min_distance_index = np.argmin(distances)
-
-    # Retrieve row with smallest cosine distance
-    row_with_min_distance = df.iloc[min_distance_index]
-    song = row_with_min_distance["song_title"]
-    return song
 
 sentiment_list = []
 
@@ -106,19 +85,39 @@ with open("./nounlist.csv", "r") as file:
 
 situation_dict = find_max_values(situation_list, columns)
 
-def prompt_user():
-    situation_keywords = ["home"]
-    sentiment_keywords = ["happy"]
-    # situation_keywords = input("Input some keywords related to the situation comma seperated: ").split(",")
-    #sentiment_keywords = input("Input some keywords related to the sentiment/feelings comma seperated: ").split(",")
-    
+def get_keyword_weights_normalized(keyword, distances, columns):
+    weights = {}
+    for column in columns:
+        ## first we get the distance, then we need to convert it to a weight
+        distance = distances[column]
+        distance_to_min = abs(get_cosine(distance["min"], keyword))
+        if distance["distance"] == 0.0:
+            normalized_weight = 1.0
+        else:
+            normalized_weight = (distance["distance"] - distance_to_min) / distance["distance"]
+        weights[column] = normalized_weight
+    return weights
+
+## recommends a song based a vector, and the distance to a songs vector
+def recommend_song(song_vector):
+    song_vector = np.array(list(song_vector.values()))
+    distances = cosine_distances(normalized_songs, [song_vector])
+    min_distance_index = np.argmin(distances)
+    # Retrieve row with smallest cosine distance
+    row_with_min_distance = df.iloc[min_distance_index]
+    # song = row_with_min_distance["song_title"]
+    as_dict = row_with_min_distance.__dict__
+    return row_with_min_distance["song_title"]
+
+def is_words_valid(situation_keywords, sentiment_keywords):
     for sentiment_keyword in sentiment_keywords:
         if sentiment_keyword not in sentiment_list:
-            print("You used a sentiment keyword not in the list, please do try agian good sir")
+            raise ValueError("Invalid sentiment keyword: " + sentiment_keyword)
     for situation_keyword in situation_keywords:
         if situation_keyword not in situation_keywords:
-            print("You used a situation keyword not in the list, please do try agian good sir")
-    return (situation_keywords, sentiment_keywords)
+            raise ValueError("Invalid situation keyword: " + situation_keyword)
+
+    return (situation_keywords, sentiment_keywords)    
 
 print("Welcome to the greatest song recommendation system in existence! I will be your guide on your journey to your musical discovery!")
 print("At the moment, we only support select words, very sorry!")
@@ -132,26 +131,85 @@ print("                       (    `-''  __``-'    /")
 print("                        ``-....-''  ``-..-''")
 print("\n")
 
-while True:
-    situation_keywords, sentiment_keywords = prompt_user()
+
+
+app = Flask(__name__)
+
+@app.route('/endpoint', methods=['POST'])
+def post_endpoint():
+    data = request.get_json()
+    situation_keywords = data.get('situation_keywords')
+    sentiment_keywords = data.get('sentiment_keywords')
     weights_summed = {}
+    try:
+        is_words_valid(situation_keywords, sentiment_keywords)
+    except ValueError as e:
+        return str(e), 400
     ## initilaize
     for column in columns:
         weights_summed[column] = 0
     ## sum up
     for keyword in situation_keywords:
-        keyword_weights = get_keyword_weights(keyword, situation_dict, columns)
+        keyword_weights = get_keyword_weights_normalized(keyword, situation_dict, columns)
         for key, weight in keyword_weights.items():
             weights_summed[key] += weight
     for keyword in sentiment_keywords:
-        keyword_weights = get_keyword_weights(keyword, sentiment_dict, columns)
+        keyword_weights = get_keyword_weights_normalized(keyword, sentiment_dict, columns)
         for key, weight in keyword_weights.items():
             weights_summed[key] += weight
     ## normalize
     for weight in weights_summed:
-        weights_summed[weight] = weights_summed[weight] / len(situation_keywords + sentiment_keywords)
-    recommend_song(weights_summed)    
+        weights_summed[weight] = weights_summed[weight] / len(situation_keywords) + len(sentiment_keywords)
+    if len(weights_summed) < 0:
+        return "no valid song", 400
+    return recommend_song(weights_summed), 200
+        
+
+if __name__ == '__main__':
+    app.run()
+    
+    
+## run locally:
+
+def prompt_user():
+    situation_keywords = ["home", "car"]
+    sentiment_keywords = ["happy", "sad"]
+    situation_keywords = input("Input some keywords related to the situation comma seperated: ").split(",")
+    sentiment_keywords = input("Input some keywords related to the sentiment/feelings comma seperated: ").split(",")
+    for sentiment_keyword in sentiment_keywords:
+        if sentiment_keyword not in sentiment_list:
+            print("You used a sentiment keyword not in the list, please do try agian good sir")
+            return [], []
+    for situation_keyword in situation_keywords:
+        if situation_keyword not in situation_keywords:
+            print("You used a situation keyword not in the list, please do try agian good sir")
+            return [], []
+    return (situation_keywords, sentiment_keywords)
+
+# while True:
+#     situation_keywords, sentiment_keywords = prompt_user()
+#     weights_summed = {}
+#     if len(situation_keywords) + len(sentiment_keywords) > 0:
+#         ## initilaize
+#         for column in columns:
+#             weights_summed[column] = 0
+#         ## sum up
+#         for keyword in situation_keywords:
+#             keyword_weights = get_keyword_weights_normalized(keyword, situation_dict, columns)
+#             for key, weight in keyword_weights.items():
+#                 weights_summed[key] += weight
+#         for keyword in sentiment_keywords:
+#             keyword_weights = get_keyword_weights_normalized(keyword, sentiment_dict, columns)
+#             for key, weight in keyword_weights.items():
+#                 weights_summed[key] += weight
+#         ## normalize
+#         for weight in weights_summed:
+#             weights_summed[weight] = weights_summed[weight] / len(situation_keywords) + len(sentiment_keywords)
+#         if len(weights_summed) > 0:
+#             song = recommend_song(weights_summed)
+#             print(song)
             
     
     # Do something with the keywords
     # You can add your logic here to process or analyze the input keywords
+    
